2c85562142fe480f403b9dcb254a042b4a3c5740
Jakub Bujak <kuba.bujak0@wp.pl>
2023-04-26 12:48:02 +0200

    Created Modal component

---
+++ client/src/components/ModalShow.js
@@ -0,0 +1,28 @@
+function ModalShow({ onClose ,onDelete, eventId }) {
+	return (
+		<div>
+			<div className="modal-header">
+				<div className="modal-title">Czy na pewno chcesz usunąć wydarzenie</div>
+				<div>
+					<span className="close-button" onClick={onClose}>
+						x
+					</span>
+				</div>
+			</div>
+			<div className="modal-desc">
+					<p>Tego kroku nie można cofnąć</p>
+			</div>
+			<div className="modal-footer">
+				<button className="event-btn edit-btn" onClick={onClose}>
+					Zamknij
+				</button>
+				<button className="event-btn delete-btn" onClick={() => onDelete(eventId)}>
+					Usuń
+				</button>
+			</div>
+		</div>	
+	)
+	
+}
+
+export default ModalShow
