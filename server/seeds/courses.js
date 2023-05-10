a0f8ba2477f6068c78bb633a5874e65483cc3682
Jakub Bujak <kuba.bujak0@wp.pl>
2023-05-10 10:57:47 +0200

    Created new seed to courses

---
+++ server/seeds/courses.js
@@ -0,0 +1,74 @@
+module.exports = [
+	{
+		title: "Kurs Tenisa dla początkujących",
+		dateFrom: "2023-06-01",
+		dateTo: "2023-06-30",
+		hoursFrom: "9:00",
+		hoursTo: "11:00",
+		language: "polski",
+		level: "początkujący",
+		location: "Centrum Tenisowe, ul. Zielona 15, Warszawa",
+		coach: "Jan Kowalski",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	},
+	{
+		title: "Kurs Pływania dla zaawansowanych",
+		dateFrom: "2023-06-15",
+		dateTo: "2023-07-15",
+		hoursFrom: "17:00",
+		hoursTo: "19:00",
+		language: "polski",
+		level: "zaawansowany",
+		location: "Pływalnia Olimpijska, ul. Czerwona 10, Kraków",
+		coach: "Anna Nowak",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	},
+	{
+		title: "Kurs Jogi dla początkujących",
+		dateFrom: "2023-07-01",
+		dateTo: "2023-08-31",
+		hoursFrom: "10:00",
+		hoursTo: "12:00",
+		language: "angielski",
+		level: "początkujący",
+		location: "Centrum Jogi, ul. Warszawska 10, Wrocław",
+		coach: "Hans Schmidt",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	},
+	{
+		title: "Kurs CrossFitu dla średniozaawansowanych",
+		dateFrom: "2023-08-01",
+		dateTo: "2023-08-31",
+		hoursFrom: "19:00",
+		hoursTo: "21:00",
+		language: "polski",
+		level: "średniozaawansowany",
+		location: "CrossFit Arena, ul. Grodzka 5, Kraków",
+		coach: "Maria Rodriguez",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	},
+	{
+		title: "Kurs Siatkówki Plażowej dla początkujących",
+		dateFrom: "2023-09-01",
+		dateTo: "2023-09-30",
+		hoursFrom: "14:00",
+		hoursTo: "16:00",
+		language: "polski",
+		level: "początkujący",
+		location: "Plaża Miejska, ul. Wawelska 10, Warszawa",
+		coach: "John Smith",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	},
+	{
+		title: "Kurs Jazdy na Rolkach dla zaawansowanych",
+		dateFrom: "2023-10-01",
+		dateTo: "2023-11-30",
+		hoursFrom: "18:00",
+		hoursTo: "20:00",
+		language: "polski",
+		level: "zaawansowany",
+		location: "Skatepark, ul. Mickiewicza 20, Poznań",
+		coach: "Andrew Johnson",
+		img: "https://images.unsplash.com/photo-1571019613454-1cb2f99b2d8b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+	}	
+]
