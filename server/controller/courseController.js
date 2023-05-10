5adca73a88115ae350207c27c7e201cb9b4476e8
Jakub Bujak <kuba.bujak0@wp.pl>
2023-05-10 23:58:34 +0200

    Added creat, edit, delete function into server

--- server/controller/courseController.js
+++ server/controller/courseController.js
@@ -25,4 +25,77 @@ const addUser = asyncHandler(async (req, res) => {
 
 })
 
-module.exports = { getCourses, addUser };
+const createCourse = asyncHandler(async (req, res) => {
+    const {  title, location, img, dateFrom, dateTo, hoursFrom, hoursTo, level, coach, language } = req.body;
+    if (!title || !location || !img || !dateFrom || !dateTo || !hoursFrom || !language || !hoursTo || !level || !coach) {
+        res.status(400);
+        throw new Error("Wszystkie pola są wymagane");
+    }
+
+    const course = await Course.create({
+        title, 
+        location,
+        img,
+        dateFrom,
+        dateTo,
+        hoursFrom,
+        hoursTo,
+        level,
+        coach,
+        language
+    });
+
+    if (course) {
+        res.status(201).json(course);
+    } else {
+        res.status(400);
+        throw new Error("Pola wydarzenia nie są poprawne")
+    }
+
+    res.send({ message: "Stworzono kurs" } )
+})
+
+const getCourse = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const course = await Course.findById(id);
+    if (course) {
+        res.status(200).json(course);
+    } else {
+        res.status(400);
+        throw new Error("Kurs nie został znaleziony");
+    }
+})
+
+const editCourse = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const {  title, location, img, dateFrom, dateTo, hoursFrom, hoursTo, level, coach, language } = req.body;
+
+    if (!title || !location || !img || !dateFrom || !dateTo || !hoursFrom || !language || !hoursTo || !level || !coach) {
+        res.status(400);
+        throw new Error("Wszystkie pola są wymagane");
+    }
+
+    const course = await Course.findByIdAndUpdate(id, req.body);
+
+    if (course) {
+        res.status(200).json(course)
+    } else {
+        res.status(400);
+        throw new Error("Nie udało się edytować kursu");
+    }
+
+    res.send( { message: "Edytowano kurs" } )
+})
+
+const deleteCourse = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const deletedCourse = await Course.findByIdAndDelete(id);
+    if (deleteCourse) {
+        res.status(200).json({ message: "Pomyślnie usunięto" })
+    } else {
+        res.status(400);
+        throw new Error("Nie udało się usunąć kursu");
+    }
+})
+
+module.exports = { getCourses, addUser, createCourse, getCourse, editCourse, deleteCourse };
